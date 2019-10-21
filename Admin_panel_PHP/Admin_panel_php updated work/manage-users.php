d4d19c1c293fb0b38ed7e4095f3742c41ddec7b4
moidahmed <55665636+moidahmed@users.noreply.github.com>
2019-10-21 08:57:59 +0500

    Add files via upload

---
+++ Admin_panel_PHP/Admin_panel_php updated work/manage-users.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+<?php include('include/header.php');?>
+<?php include('include/sidemenu.php');?>
+  
+	 					<div class="main">
+								<table class="table table-bordered"  style="font-size: 14px;"> 
+									<thead class="thead-dark">
+										  
+										<tr>
+		
+											<th>#</th>
+											<th>User Name</th>
+											<th>Email </th>
+											<th>Contact No</th>
+											<th>Shipping Address</th>
+ 
+											
+										</tr>
+									</thead>
+									<tbody>
+
+										<?php  
+										$conn = mysqli_connect("localhost", "root", "", "userregister");
+										$query=mysqli_query($conn,"select * from user");
+											 
+											while($row=mysqli_fetch_array($query))
+											{
+											?>									
+										<tr>
+											<td><?php echo ($row['id']);?></td>
+											<td><?php echo ($row['name']);?></td>
+											<td><?php echo ($row['email']);?></td>
+											<td><?php echo ($row['contactno']);?></td>
+											<td><?php echo ($row['shippingaddress']);?></td>
+										<?php } ?>
+
+										</tr>
+ 											
+									
+						
+										<!--<tr>
+											<td>1</td>
+											<td>Moid Ahmed</td>
+											<td>moidahmed0@gmail.com</td>
+											<td>03363057355</td>
+											<td>gulshan iqbal , isphanni road karachi</td>
+											 
+										</tr> -->
+										 
+										 
+
+									</tbody>
+								</table>
+				</div>
+<?php include('include/footer.php');?> 
+
+</body>
+
+</html>
---
+++ Admin_panel_PHP/Admin_panel_php updated work/update_product.php
@@ -0,0 +1,249 @@
+
+
+
+
+	<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	 
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>  
+</head>
+<body>
+	
+<?php include('include/header.php');?>
+<?php include('include/sidemenu.php');?> 
+
+	<?php
+$conn = mysqli_connect("localhost", "root", "", "userregister");
+
+	if (isset($_GET['update_product'])) {
+
+		$get_id = $_GET['update_product'];
+
+		$get_product = "select * from products where product_id='$get_id'";
+
+    //$get_product = "SELECT products.*,category.category_name,brands.brand_name from products join category on category.category_id=products.categorys_id join brands on brands.brand_id=products.brands_id where product_id='$get_id'";
+
+		$run_product = mysqli_query($conn, $get_product);
+
+	 
+		$row_product  = mysqli_fetch_array($run_product);
+
+
+					              $product_id = $row_product['product_id'];
+                        $product_name = $row_product['product_name'];
+                        $product_price = $row_product['product_price'];
+                        $product_description = $row_product['product_description'];
+                        $stock = $row_product['stock'];
+                        $product_image = $row_product['product_image'];
+                        $categorys_id = $row_product['categorys_id'];
+                        //$brand_name = $row['brand_name'];
+                        //$category_name = $row['category_name'];
+                        $brands_id = $row_product['brands_id'];
+
+
+	 $get_category = "select * from category where category_id='$categorys_id'";
+
+	  $run_category = mysqli_query($conn, $get_category);
+
+	  $row_category = mysqli_fetch_array($run_category);
+
+		$category_name = $row_category['category_name'];
+
+
+
+ //$get_brand="select * from brands INNER JOIN products ON brands.brand_id = products.brands_id ";
+
+
+		$get_brand = "select * from brands where brand_id='$brands_id'";
+
+		$run_brand = mysqli_query($conn, $get_brand);
+
+		$row_brand = mysqli_fetch_array($run_brand);
+    
+		$brand_name = $row_brand['brand_name'];
+	}
+	?>
+
+
+  <div class="main">
+<form class="form-horizontal" method="POST"  enctype="multipart/form-data" >
+
+
+ 
+ 
+ <div class="form-group">
+  <label class="col-md-4 control-label"  >Category</label>  
+  <div class="col-md-4" class="form-group">
+  
+  <select class="form-control input-md" name="categorys_id" > 
+                     <option value="<?php echo $product_id;?>" ><?php echo $category_name;?></option>
+
+               <?php  
+                   
+                  $query=mysqli_query($conn,"select * from category ");
+                       
+                       while($row_category=mysqli_fetch_array($query))
+                      {
+                       
+                       $category_id = $row_category['category_id'];
+                      $category_name = $row_category['category_name'];    
+                      echo "<option value='$category_id'>$category_name</option>";         
+                   
+                     
+                    } ?>
+
+ 
+                    </select>
+   </div>
+    </div> 
+
+
+ 
+
+
+ 
+<div class="form-group">
+  <label class="col-md-4 control-label" >Product Name</label>  
+  <div class="col-md-4">
+  <input placeholder="Product Name" class="form-control input-md" name="product_name"  type="text" value="<?php echo $product_name; ?> ">
+    
+  </div>
+</div>
+
+
+
+
+
+      <div class="form-group">
+           <label class="col-md-4 control-label" >Brand Name</label>
+             <div class="col-md-4" class="form-group">
+
+           <select  class="form-control input-md" name="brands_id">
+            <option value="<?php echo $product_id;?>"><?php echo $brand_name;?></option>
+            <?php
+            $get_brands = "select * from brands";
+
+            $run_brands = mysqli_query($conn, $get_brands);
+
+            while ($row   = mysqli_fetch_array($run_brands)) {
+
+              $brand_id = $row['brand_id'];
+              $brand_name = $row['brand_name'];
+
+              echo "<option value='$brand_id'>$brand_name</option>";
+            } ?>
+
+          </select>
+        </div>
+</div>
+
+
+
+
+
+
+
+ 
+<div class="form-group">
+  <label class="col-md-4 control-label" >Product Price</label>  
+  <div class="col-md-4">
+  <input placeholder="Product Price" class="form-control input-md" name="product_price"  type="text" value="<?php echo $product_price; ?>">
+    
+  </div>
+</div>
+ 
+
+ 
+<div class="form-group">
+  <label class="col-md-4 control-label">Product Description</label>
+  <div class="col-md-4">                     
+    <textarea class="form-control" name="product_description"> <?php echo $product_description; ?>  </textarea>
+  </div>
+</div>
+
+
+
+
+<div class="form-group">
+<label class="col-md-4 control-label">Product Available</label>
+<div class="col-md-4">
+<select class="form-control input-md"  name="stock"  >
+<option value="<?php echo $stock;?>"> <?php echo $stock;?></option>
+ <option value="In Stock">In Stock</option>
+<option value="Out of Stock">Out of Stock</option> 
+</select>
+</div>
+</div>
+
+ 
+ 
+<div class="form-group" >
+  <label class="col-md-4 control-label">Product Image</label>
+  <div class="col-md-4">
+      <input class="input-file" type="file" name="product_image" value="product_image" ><img src="product_images/<?php echo $product_image; ?>" >  
+    <!-- <input class="input-file" type="file" name="product_image" > <img src="product_images/<?php //echo $update_id;?>/<?php //echo  $product_image;?>" > -->
+   <!--<input type="file" class="input-file" name="product_image"><img src="http://localhost/admin-panel-comfort-mart/product_images/<?php// echo $product_image; ?>" width="60"
+          height="60"/>  -->
+
+    <br><br>
+     
+    <button class="btn btn-primary control-label" name="updateProduct" type="submit"  >Product Update</button>
+
+    
+  </div>
+</div>
+
+ 
+<?php
+ $conn = mysqli_connect("localhost", "root", "", "userregister"); 
+
+  if (isset($_POST['updateProduct'])) {
+
+      //$update_id=$_POST['product_id'];
+      //$update_id = 'product_id';
+
+      $update_id = $product_id;
+
+        $product_name=$_POST['product_name'];
+        // $brands_id=$_POST['brands_id'];
+            $brands_id=$_POST['brands_id'];
+            $product_price=$_POST['product_price'];
+            $product_description=$_POST['product_description'];
+            $categorys_id=$_POST['categorys_id'];
+            $stock = $_POST['stock'];
+            $product_image=$_FILES['product_image']['name'];
+            $tmp = $_FILES['product_image']['tmp_name'];
+            move_uploaded_file($tmp, "product_images/$product_image");
+            
+
+ 
+          $update_product= "UPDATE products set product_name='$product_name' , brands_id ='$brands_id', product_price='$product_price', product_description='$product_description', categorys_id=' $categorys_id' , stock = '$stock' , product_image = '$product_image' where product_id='$update_id'";
+
+
+    $run_product = mysqli_query($conn, $update_product);
+
+    if ($run_product) {
+
+      echo "<script>alert('Product has been updated!')</script>";
+      echo "<script>window.open('manage-products.php', '_self')</script>";
+
+    }
+    else{
+
+              echo "<script>alert('Product has not been updated!')</script>";
+      }
+    }
+
+?>
+
+
+</body>
+</html>
+
+
+
+
