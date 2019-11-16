ced79a43f2bd66e7ef8d3f942ae3eaa1d5d8f47d
SYED MUHAMMAD SADIQ <37629461+SyedMuhammadSadiq@users.noreply.github.com>
2019-11-16 17:07:58 +0500

    included in cmfronted folder

--- CM Frontend/category.php
+++ CM Frontend/category.php
@@ -66,6 +66,96 @@ if(isset($_GET['action']) && $_GET['action']=="add"){
 	}
 
 ?>
+<<<<<<< HEAD
+=======
+		<div class="w3l_banner_nav_right">
+			<div class="w3l_banner_nav_right_banner7">
+				<h3>Best Deals For New Products<span class="blink_me"></span></h3>
+			</div>
+			<div class="w3ls_w3l_banner_nav_right_grid w3ls_w3l_banner_nav_right_grid_sub">
+				<h3>Beverages</h3>
+				<div class="w3ls_w3l_banner_nav_right_grid1">
+					<h6>soft drinks</h6>
+					<div class="col-md-3 w3ls_w3l_banner_left">
+						<div class="hover14 column">
+						<div class="agile_top_brand_left_grid w3l_agile_top_brand_left_grid">
+							<div class="agile_top_brand_left_grid_pos">
+								<img src="images/offer.png" alt=" " class="img-responsive" />
+							</div>
+							<div class="agile_top_brand_left_grid1">
+								<figure>
+									<div class="snipcart-item block">
+										<div class="snipcart-thumb">
+											<a href="single.html"><img src="images/49.png" alt=" " class="img-responsive" /></a>
+											<p>Olper Milk (250 ml)</p>
+											<h4>RS 35 </h4>
+										</div>
+										<div class="snipcart-details">
+											<form action="#" method="post">
+												<fieldset>
+													<input type="hidden" name="cmd" value=" " />
+													<input type="hidden" name="add" value=" " />
+													<input type="hidden" name="business" value=" " />
+													<input type="hidden" name="item_name" value="olper milkk" />
+													<input type="hidden" name="amount" value=" " />
+													<input type="hidden" name="discount_amount" value=" " />
+													<input type="hidden" name="return" value=" " />
+													<input type="hidden" name="cancel_return" value=" " />
+													<input type="submit" name="submit" value="Add to cart" class="button" />
+												</fieldset>
+											</form>
+										</div>
+									</div>
+								</figure>
+							</div>
+						</div>
+						</div>
+					</div>
+					
+					<div class="col-md-3 w3ls_w3l_banner_left">
+						<div class="hover14 column">
+						<div class="agile_top_brand_left_grid w3l_agile_top_brand_left_grid">
+							<div class="agile_top_brand_left_grid_pos">
+								<img src="images/offer.png" alt=" " class="img-responsive" />
+							</div>
+							<div class="agile_top_brand_left_grid1">
+								<figure>
+									<div class="snipcart-item block">
+										<div class="snipcart-thumb">
+											<a href="single.html"><img src="images/56.png" alt=" " class="img-responsive" /></a>
+											<p>Olper 1 Ltr</p>
+											<h4>RS 135</h4>
+										</div>
+										<div class="snipcart-details">
+											<form action="#" method="post">
+												<fieldset>
+													<input type="hidden" name="cmd" value=" " />
+													<input type="hidden" name="add" value=" " />
+													<input type="hidden" name="business" value=" " />
+													<input type="hidden" name="item_name" value="red bull energy drink" />
+													<input type="hidden" name="amount" value=" " />
+													<input type="hidden" name="discount_amount" value=" " />
+													<input type="hidden" name="return" value=" " />
+													<input type="hidden" name="cancel_return" value=" " />
+													<input type="submit" name="submit" value="Add to cart" class="button" />
+												</fieldset>
+											</form>
+										</div>
+									</div>
+								</figure>
+							</div>
+						</div>
+						</div>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+<!-- //banner -->
+<?php include('footer.php'); ?>
+>>>>>>> 0ececcadaca322dc27b2724c3f7f00f3c63786a8
 
     <div class="body-content outer-top-vs" id="top-banner-and-menu">
         <!-- Categories -->
@@ -144,6 +234,7 @@ if(isset($_GET['action']) && $_GET['action']=="add"){
 	                                        <ul class="list-unstyled">
 	                                            <li  class="lnk wishlist" >
 
+<<<<<<< HEAD
 	                                         
 	                                                <a class="add-to-cart" href="category.php?page=product&action=add&id=<?php echo $value->id; ?>" title="Add to cart"><i class="fa fa-shopping-cart"></i> </a>
 	                                            </li>
@@ -176,3 +267,7 @@ if(isset($_GET['action']) && $_GET['action']=="add"){
     	<?php } ?>
     </div>
 <?php include('layouts/footer.php'); ?>
+=======
+</body>
+</html>
+>>>>>>> 0ececcadaca322dc27b2724c3f7f00f3c63786a8
--- CM Frontend/index.php
+++ CM Frontend/index.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php require('./autoload.php'); ?>
 <?php include('layouts/header.php'); ?>
 <?php 
@@ -1428,3 +1429,120 @@
    
 
 <?php include('layouts/footer.php'); ?>
+=======
+<?php include('header.php'); ?>
+
+
+<?php include('left-navbar.php') ?>
+		<div class="w3l_banner_nav_right">
+			<section class="slider">
+				<div class="flexslider">
+					<ul class="slides">
+						<li>
+							<div class="w3l_banner_nav_right_banner">
+								<h3>Make your <span>food</span> with Spicy.</h3>
+								<div class="more">
+									<a href="products.php" class="button--saqui button--round-l button--text-thick" data-text="Shop now">Shop now</a>
+								</div>
+							</div>
+						</li>
+						<li>
+							<div class="w3l_banner_nav_right_banner1">
+								<h3>Make your <span>food</span> with Spicy.</h3>
+								<div class="more">
+									<a href="products.php" class="button--saqui button--round-l button--text-thick" data-text="Shop now">Shop now</a>
+								</div>
+							</div>
+						</li>
+						<li>
+							<div class="w3l_banner_nav_right_banner2">
+								<h3>upto <i>50%</i> off.</h3>
+								<div class="more">
+									<a href="products.php" class="button--saqui button--round-l button--text-thick" data-text="Shop now">Shop now</a>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</section>
+			<!-- flexSlider -->
+				<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property="" />
+				<script defer src="js/jquery.flexslider.js"></script>
+				<script type="text/javascript">
+				$(window).load(function(){
+				  $('.flexslider').flexslider({
+					animation: "slide",
+					start: function(slider){
+					  $('body').removeClass('loading');
+					}
+				  });
+				});
+			  </script>
+			<!-- //flexSlider -->
+		</div>
+		<div class="clearfix"></div>
+	</div>
+<!-- top-brands -->
+	<div class="top-brands">
+		<div class="container">
+			<h3>Products</h3>
+			<div class="agile_top_brands_grids">
+	
+			<?php
+			include('classes/config.php');
+
+				$query = "SELECT * FROM products ORDER BY product_name ASC";
+				$result = mysqli_query($connect, $query);
+				if(mysqli_num_rows($result) > 0)
+				{
+					while($row = mysqli_fetch_array($result))
+					{
+                        $product_name = $row['product_name'];
+                        $product_price = $row['product_price'];             
+                        $product_image = $row['product_image'];
+				?>
+				<div class="col-md-3 top_brand_left">
+
+					<div class="hover14 column">
+						<div class="agile_top_brand_left_grid">
+							
+							<div class="agile_top_brand_left_grid1">
+								<figure>
+									<div class="snipcart-item block" >
+										<div class="snipcart-thumb">
+											<a href="single.php"><img title=" " alt=" " src="images/<?php echo $product_image; ?>"width="100" height="150" /></a>		
+											<h5><?php echo $product_name; ?></h5>
+											<h4>Rs. <?php echo $product_price; ?> </h4>
+										</div>
+										<div class="snipcart-details top_brand_home_details">
+											<form action="checkout.php" method="post">
+												<fieldset>
+													<input type="hidden" name="cmd" value="_cart" />
+													<input type="hidden" name="add" value="1" />
+													<input type="hidden" name="business" value=" " />
+													<input type="hidden" name="item_name" value="<?php echo $product_name; ?>" />
+													<input type="hidden" name="amount" value="<?php echo $product_price; ?>" />
+																						
+													<input type="hidden" name="return" value=" " />
+													<input type="hidden" name="cancel_return" value=" " />
+													<input type="submit" name="submit" value="Add to cart" class="button" />
+												</fieldset>
+													
+											</form>
+									
+										</div>
+									</div>
+								</figure>
+							</div>
+						</div>
+					</div>
+				</div>		<?php }} ?>
+
+
+					<div class="clearfix"> </div>
+		</div>
+	</div>
+</div>
+					
+<?php include('footer.php'); ?>
+>>>>>>> 0ececcadaca322dc27b2724c3f7f00f3c63786a8
